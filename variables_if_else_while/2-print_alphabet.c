3377a8d034af47ae0b36c25fcda7c0692f9c689d
Macj04 <miguelcoa49@gmail.com>
2023-02-16 16:12:53 -0800

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+
+/**
+ * main - prints the alphabet with putchar
+ *
+ * return: 0 if success
+ */
+
+int main(void)
+{
+	char m;
+	char M;
+
+	for (m = 'a'; m <= 'z'; a++)
+		putchar(m);
+	for (M = 'A'; M <= 'Z'; A++)
+		putchar(M);
+	
+
+	putchar('\n');
+	return (0);
+}
