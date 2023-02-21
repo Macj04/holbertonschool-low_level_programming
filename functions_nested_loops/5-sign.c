303f549dc383aac8dd5f484e0a5de7a0d7ec254b
Macj04 <miguelcoa49@gmail.com>
2023-02-21 09:18:59 -0800

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number depending n
+ * @n: number
+ * Return: 1 is positive, -1 is negative or 0 is zero
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(n + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1)
+	}
+}
