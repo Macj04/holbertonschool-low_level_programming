8167438a6ff359ee8c00e0259df6f664c928c75b
Macj04 <miguelcoa49@gmail.com>
2023-02-23 05:18:36 -0800

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *more_numbers(void) - print numbers from 0 to 14 10 times
+ *Return: Always 0
+*/
+void more_numbers(void)
+{
+	int reps;
+	int num;
+
+	for (reps = 0; reps <= 9; reps++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar(num / 10 + '0');
+			}
+			_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
