4ba2bdedd2821deb45e079ea2da9e2e2533a9bcf
Macj04 <miguelcoa49@gmail.com>
2023-02-23 03:41:54 -0800

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,9 +3,10 @@
  * mul(int a, int b) - function that multiplies two integers
  * @a:digit a
  * @b:digit b
- * Return: multiplication of two numbers
+ * Return: multiplication of two number
  */
 int mul(int a, int b)
 {
 	return (a * b);
 }
+
