1e36fd9bbd55525a0634c856e193ec4cbe389b05
Macj04 <miguelcoa49@gmail.com>
2023-02-23 04:07:43 -0800

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char num = 48;
+
+	while(num < 58)
+	{
+		write(1, &num, 1), num++;
+	}
+	_putchar(10);
+}
