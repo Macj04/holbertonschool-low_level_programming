77bd205b5c41637378ebf5ae0383e54cc3ae1616
Macj04 <miguelcoa49@gmail.com>
2023-02-23 05:40:49 -0800

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *more_numbers(void) - print numbers from 0 to 14 10 times
+ *more_numbers - print numbers from 0 to 14 10 times
  *Return: Always 0
 */
 void more_numbers(void)
