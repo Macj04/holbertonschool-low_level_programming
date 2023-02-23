5b9a5a7386c47c80d328f0acfe9f2196b058a906
Macj04 <miguelcoa49@gmail.com>
2023-02-23 11:39:20 -0800

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - print the numbers  and use Fizz or Buzz instead of 3 and 5 multiples
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 1; m <= 100; m++)
+	{
+		if (m % 3 == 0 && m % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (m % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (m % 5 == 0)
+		{
+			printf("%d ", m);
+		}
+	}
+	printf("\n");
+	return (0);
+}
