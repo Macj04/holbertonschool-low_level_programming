708090ca81bcb1e631eea2aaa53a37776d5576ad
Macj04 <miguelcoa49@gmail.com>
2023-02-28 05:00:12 -0800

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - print every other character starting with the first character
+ * @s:string
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int m;
+
+	for (m = 0; str[m]; m++)
+	{
+		if (m % 2 == 0)
+			_putchar(str[m]);
+	}
+	_putchar(10);
+}
