bc0c8a511be31af14fb3ec58fbbdc7b7f7ab16b9
Macj04 <miguelcoa49@gmail.com>
2023-03-01 14:31:09 -0800

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings
+ * @dest:output
+ * @src:input
+ * Return:dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int m;
+	int c;
+
+	for (m = 0; dest[m] != '\0'; m++)
+	{
+	}
+	for (c = 0; (src[c] != '\0' && c < n); m++, c++)
+	{
+		dest[m] = src[c];
+	}
+	return (dest);
+}
