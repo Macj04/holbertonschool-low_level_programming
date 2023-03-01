2f4984f3f22c3335bdfda3828e14f61956175e86
Macj04 <miguelcoa49@gmail.com>
2023-03-01 15:44:21 -0800

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int m = 0;
+	int m;
 
-	while (s1[m] || s2[m])
+	for (m = 0; s1[m] && s2[m]; m++)
 	{
 		if (s1[m] != s2[m])
 			return (s1[m] - s2[m]);
 	}
-	m++;
 	return (0);
 }
