c230475bb72879d37117a9dcd65473d9efe84eee
Macj04 <miguelcoa49@gmail.com>
2023-03-02 16:52:41 -0800

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,15 +9,19 @@ int _atoi(char *s)
 	int m = 0;
 	int c = 1;
 
-	while (*s != '\n')
+	while (*s != '\n')/*loop until the end of the string is reached*/
 	{
 		if (*s == '-')
 		{
 			c = c * -1;
 		}
+		else if (*s >= 48 && *s <= 57)
+		{
+			m = m * 10 + (*s - '0');
+		}
 		else if (m > 0)
 		{
-			break;
+			break;/*if end of number is reached*/
 		}
 		s++;
 	}
