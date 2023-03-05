dd564537acb801df39177cbef3d5148f1ed9b991
Macj04 <miguelcoa49@gmail.com>
2023-03-05 10:28:04 -0800

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s:string
+ * Return Always 0
+ */
+char *cap_string(char *s)
+{
+	int m;
+	int c;
+	int se[] = {125, 123, 63, 59, 46, 44, 41, 40, 34, 33, 32, 10, 9};
+
+	for (c = 0; se[c]; c++)
+	{
+		for (m = 0; s[m]; m++)
+		{
+			if (m == 0)
+				if (s[m] >= 97 && s[m] <= 122)
+					s[m] = s[m] - 32;
+			if (s[m] == se[c])
+			{
+				if (s[m + 1] >= 97 && s[m + 1] <= 122)
+					s[m + 1] = s [m + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
