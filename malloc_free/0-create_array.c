28230dd81e51352490c22021d17b772f6df09a00
Macj04 <miguelcoa49@gmail.com>
2023-03-13 15:09:48 -0700

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *create_array - creates an array initializes with a specific char
+ * @size:size
+ * @c:character
+ * Return: Always 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ray;
+	unsigned int m;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ray = malloc (size * sizeof(char));
+
+	if (ray == NULL)
+	{
+		return (NULL);
+	}
+	for (m = 0; m < size; m++)
+	{
+		ray[m] = c;
+	}
+	return (ray);
+}
