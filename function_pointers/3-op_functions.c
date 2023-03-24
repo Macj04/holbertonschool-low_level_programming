80ebab12f405bcaeda87cbc6e9b30706fb51bbb0
Macj04 <miguelcoa49@gmail.com>
2023-03-24 15:50:15 -0700

    program that performs simple operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - return the sum
+ * @a:argument1
+ * @b:argument2
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the difference
+ * @a:argument1
+ * @b:argument2
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - return the product
+ * @a:argument1
+ * @b:argument2
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - return the div
+ * @a:argument1
+ * @b:argument2
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return the mod
+ * @a:argument1
+ * @b:argument2
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
