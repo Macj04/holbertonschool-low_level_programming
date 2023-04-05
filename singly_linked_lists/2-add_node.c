661d27d955bff5a018a5b28c72126b7ca630e2b3
Macj04 <miguelcoa49@gmail.com>
2023-04-05 16:59:12 -0700

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_note - adds a new node at the beginning of a list_t
+ * add_node - adds a new node at the beginning of a list_t
  * @head:ptr
  * @str:string
  * Return: address of the new element or NULL
